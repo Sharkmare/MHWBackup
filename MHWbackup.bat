2ec002eae29fd25ded2977c3e9325b082486f7c9
Sharkmare <34294231+Sharkmare@users.noreply.github.com>
2018-10-17 01:22:19 +0200

    V1.1
    Added built in steamID setting
    Added errorhandling for wrong steamID

--- MHWbackup.bat
+++ MHWbackup.bat
@@ -1,6 +1,10 @@
 @ECHO off
+:START
 TITLE MHW Backup Script
-SET STEAMID=STEAMIDGOESHERE
+IF NOT EXIST SID GOTO genSID
+set /p STEAMID=<SID
+IF NOT EXIST "C:\Program Files (x86)\Steam\userdata\%STEAMID%\582010\remote" GOTO Error001
+set /p STEAMID=<SID
 Time/t>tmptime.txt
 Date/t>tmpdate.txt
 timeout/t 2>nul
@@ -20,3 +24,24 @@ ECHO LOG: Succesfully backed up file on %DATE% at %TIME% saved in %storeDir%>las
 ECHO LOG: Succesfully backed up file on %DATE% at %TIME% saved in %storeDir%>>Persistentlog.log
 TIMEOUT/t 10>nul
 exit/b
+:Error001
+
+CLS
+ECHO "ERROR: 1" MHW Savefile not found
+ECHO Possibly wrong STEAMID 
+ECHO[
+ECHO Please Delete "%CD%\SID" or manually modify your SID file to contain the correct ID
+PAUSE
+GOTO START
+:genSID
+
+copy /y NUL SID >NUL
+cls
+ECHO First time setup triggered
+ECHO[
+ECHO Please enter your SteamID
+SET/P SID="SteamID="
+set STEAMID=%SID%
+IF NOT EXIST "C:\Program Files (x86)\Steam\userdata\%STEAMID%\582010\remote" GOTO Error001
+ECHO %SID%>SID
+GOTO START
